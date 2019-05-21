10dfb9c58bd14a64051e5aa91de68ed02b2e548d
汤亮 <tangliang@powersi.com.cn>
2019-05-21 23:29:18 +0800

    处理了结果集

---
+++ src/main/java/mapper/AuthorMapper.java
@@ -0,0 +1,10 @@
+package mapper;
+
+/**
+ * @program: tbatis
+ * @description: 作者实体查询接口
+ * @author: tangliang
+ * @create: 2019-05-21 23:22
+ **/
+public interface AuthorMapper {
+}
---
+++ src/main/java/pojo/Author.java
@@ -0,0 +1,10 @@
+package pojo;
+
+/**
+ * @program: tbatis
+ * @description: 作者实体对象
+ * @author: tangliang
+ * @create: 2019-05-21 23:21
+ **/
+public class Author {
+}
